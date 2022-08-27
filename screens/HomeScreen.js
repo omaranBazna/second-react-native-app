83a44f8c48ffdfe074f246f9504f135992f71161
omaran bazna <omran4d@gmail.com>
2022-08-27 13:42:32 -0400

    add states for the Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,9 @@ import { CAMPSITES } from "../shared/campsites";
 import { PROMOTIONS } from "../shared/promotions";
 import { PARTNERS } from "../shared/partners";
 const HomeScreen = () => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [partners, setPartners] = useState(PARTNERS);
   return (
     <View>
       <Text>Home Screen</Text>
