ff49c8c4875108c6c70e5425172449d16faf6c2c
omaran bazna <omran4d@gmail.com>
2022-08-27 14:24:49 -0400

    add the contact page

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
+import { Card } from "react-native-elements";
 
 const ContactScreen = () => {
-  return <ScrollView></ScrollView>;
+  return (
+    <ScrollView>
+      <Card>
+        <Card.Title>Contact Information</Card.Title>
+        <Card.Divider />
+        <Text>1 Nucamp way</Text>
+        <Text>Seatell , 3123</Text>
+        <Text>U.S.A</Text>
+        <Text> </Text>
+        <Text>Phone:+1 312 231 2131</Text>
+        <Text>Email: someone@somedomin.somthing</Text>
+      </Card>
+    </ScrollView>
+  );
 };
 
 export default ContactScreen;
