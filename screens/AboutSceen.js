c50f6e32942be947f842b1e250492445f8e54266
omaran bazna <omran4d@gmail.com>
2022-08-27 14:07:26 -0400

    create about and contact screens

---
+++ screens/AboutSceen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+const AboutSceen = () => {
+  return <ScrollView></ScrollView>;
+};
+
+export default AboutSceen;
---
+++ screens/ContactScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+const ContactScreen = () => {
+  return <ScrollView></ScrollView>;
+};
+
+export default ContactScreen;
