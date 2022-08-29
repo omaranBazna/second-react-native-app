0d4d996d1802995c6c476cf96a40742836633531
omaran bazna <omran4d@gmail.com>
2022-08-29 16:34:08 -0400

    add data to the flat list

--- screens/CampsitesInfoScreen.js
+++ screens/CampsitesInfoScreen.js
@@ -1,7 +1,15 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { COMMENTS } from "../shared/comments";
 const CampsitesInfoScreen = ({ route }) => {
   const { campsite } = route.params;
-  return <RenderCampsite campsite={campsite} />;
+  const [comments, setComments] = useState(COMMENTS);
+  return (
+    <FlatList
+      data={comments.forEach((comment) => comment.campsiteId == campsite)}
+    />
+  );
 };
 
 export default CampsitesInfoScreen;
