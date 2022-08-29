9c517889036c1bde78c7c234a52e83fedc303a30
omaran bazna <omran4d@gmail.com>
2022-08-29 16:51:22 -0400

    finish flatList

--- screens/CampsitesInfoScreen.js
+++ screens/CampsitesInfoScreen.js
@@ -2,12 +2,50 @@ import RenderCampsite from "../features/campsites/RenderCampsite";
 import { useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { COMMENTS } from "../shared/comments";
+const styles = StyleSheet.create({
+  commentsTitle: {
+    textAlign: "center",
+    backgroundColor: "#fff",
+    fontSize: 16,
+    fontWeight: bold,
+    color: "#434840",
+    padding: 10,
+    paddingTop: 30,
+  },
+});
+
+const RenderCommentItem = ({ item }) => {
+  return (
+    <View style={styles.commentItem}>
+      <Text style={{ fontSize: 14 }}>{item.text}</Text>
+      <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+      <Text style={{ fontSize: 12 }}>
+        -- {item.author} , {item.date}
+      </Text>
+    </View>
+  );
+};
+
 const CampsitesInfoScreen = ({ route }) => {
   const { campsite } = route.params;
   const [comments, setComments] = useState(COMMENTS);
   return (
     <FlatList
       data={comments.forEach((comment) => comment.campsiteId == campsite)}
+      renderItem={RenderCommentItem}
+      keyExtractor={(item) => {
+        item.id.toString();
+      }}
+      contentContainerStyle={{
+        marginHorizontal: 20,
+        paddingVertical: 20,
+      }}
+      ListHeaderComponent={
+        <>
+          <RenderCampsite render={campsite} />
+          <Text style={styles.commentsTitle} />
+        </>
+      }
     />
   );
 };
