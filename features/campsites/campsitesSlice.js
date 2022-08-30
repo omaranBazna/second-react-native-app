5550f6dffe1b06590a4dcaf3c4f6caa95e85a4d4
omaran bazna <omran4d@gmail.com>
2022-08-30 18:06:08 -0400

    fetching the campsites data

--- features/campsites/campsitesSlice.js
+++ features/campsites/campsitesSlice.js
@@ -1,5 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+import { createSlice ,createAsyncThunk} from "@reduxjs/toolkit";
+import { baseUrl } from "../../shared/baseURL";
 
+
+export fetchCampsite=createAsyncThunk({
+    'campsites/fetchCampsites',
+    async =()=>{
+     const response =await fetch (baseUrl +"campsites") 
+    } 
+})
 const initialState = {
   campsites: [],
 };
