260074c6f74b85a3a9d008ee8a772ec0318060f3
omaran bazna <omran4d@gmail.com>
2022-08-31 18:53:19 -0400

    create favorites Slice

---
+++ features/favorites/favoritesSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { baseUrl } from "../../shared/baseUrl";
+
+export const fetchCampsites = createAsyncThunk(
+  "campsites/fetchCampsites",
+  async () => {
+    const response = await fetch(baseUrl + "campsites");
+
+    return response.json();
+  }
+);
+const initialState = {
+  isLoading: true,
+  errMess: null,
+  campsitesArr: [],
+};
+
+const campsitesSlice = createSlice({
+  name: "campsites",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCampsites.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchCampsites.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMess = null;
+      state.campsitesArr = action.payload;
+    },
+    [fetchCampsites.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMess = action.error ? action.error.message : "rejected";
+    },
+  },
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
