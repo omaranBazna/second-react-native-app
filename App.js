f6e3e9aaa12cdc73298412c8b496989565c9e8bc
omaran bazna <omran4d@gmail.com>
2022-08-30 22:05:19 -0400

    add provider to the app

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import Main from "./screens/MainComponent";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { Store } from "redux";
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </Provider>
   );
 }
