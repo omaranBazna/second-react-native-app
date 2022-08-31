6540823cf508628098ca9a580c668d3fef586867
omaran bazna <omran4d@gmail.com>
2022-08-30 22:10:58 -0400

    fetching the data when main component loaded

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -14,6 +14,13 @@ import {
 import HomeScreen from "./HomeScreen";
 import ContactScreen from "./ContactScreen";
 import AboutScreen from "./AboutScreen";
+
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchPartners } from "../features/partners/partnersSlice";
+import { fetchCampsites } from "../features/campsites/campsitesSlice";
+import { fetchPromotions } from "../features/promotions/promotionsSlice";
+import { fetchComments } from "../features/comments/commentsSlice";
 const Drawer = createDrawerNavigator();
 const screenOptions = {
   headerTintColor: "#fff",
@@ -155,6 +162,13 @@ const DirectoryNavigator = () => {
   );
 };
 const Main = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+    dispatch(fetchPromotions());
+    dispatch(fetchPartners());
+    dispatch(fetchComments());
+  }, [dispatch]);
   return (
     <View
       style={{
