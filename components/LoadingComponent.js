4f72ebeb71cf2b5fc014dfd0825d1feace7f7234
omaran bazna <omran4d@gmail.com>
2022-08-31 01:22:07 -0400

    add styles to the loading component

--- components/LoadingComponent.js
+++ components/LoadingComponent.js
@@ -1,5 +1,19 @@
-const LoadingComponent = () => {
+import { ActivityIndicator, StylesSheet, Text, View } from "react-native";
+const Loading = () => {
   return <div></div>;
 };
 
-export default LoadingComponent;
+const styles = StylesSheet.create({
+  loadingView: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  loadingText: {
+    color: "#5637DD",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
+
+export default Loading;
