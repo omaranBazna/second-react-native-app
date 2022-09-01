ed4e37481ce98bc8eaf1c7b29c4b16aa987e89c3
omaran bazna <omran4d@gmail.com>
2022-08-31 22:21:54 -0400

    finish styling

--- screens/ReservationsScreen.js
+++ screens/ReservationsScreen.js
@@ -83,14 +83,31 @@ const ReservationsScreen = () => {
       )}
 
       <View style={styles.formRow}>
-        <Button title={"conform"} />
+        <Button
+          title="Search Availability "
+          onPress={handelReservation}
+          color="#563700"
+          accessibilityLabel="Tap me to confirm availability "
+        />
       </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  formRow: {},
-  formLabel: {},
+  formRow: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+    margin: 20,
+  },
+  formLabel: {
+    fontSize: 18,
+    flex: 2,
+  },
+  formItem: {
+    flex: 1,
+  },
 });
 export default ReservationsScreen;
