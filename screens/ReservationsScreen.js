175086181df0cb03ba01137af15f94818bb10d87
omaran bazna <omran4d@gmail.com>
2022-08-31 21:19:45 -0400

    import the components

---
+++ screens/ReservationsScreen.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Switch,
+  Button,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import DateTimePicker from "@react-native-community/datetimepicker";
