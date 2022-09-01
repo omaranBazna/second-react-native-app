02a5cfd316ffbea56da7b04558cc4a4a4d8f572b
omaran bazna <omran4d@gmail.com>
2022-08-31 21:23:13 -0400

    define the component states

--- screens/ReservationsScreen.js
+++ screens/ReservationsScreen.js
@@ -9,3 +9,13 @@ import {
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
+
+const ReservationsScreen = () => {
+  const [campers, setCampers] = useState(1);
+  const [hikeIn, setHikeIn] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+  return <div></div>;
+};
+
+export default ReservationsScreen;
