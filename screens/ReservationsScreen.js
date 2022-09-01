496ef99af3115a7c86af9ae2e543075320f81213
omaran bazna <omran4d@gmail.com>
2022-08-31 21:39:08 -0400

    finish camper input

--- screens/ReservationsScreen.js
+++ screens/ReservationsScreen.js
@@ -28,12 +28,27 @@ const ReservationsScreen = () => {
   };
   return (
     <ScrollView>
-      <View style={styles.formRow}></View>
+      <View style={styles.formRow}>
+        <Text style={styles.formLabel}>Number of Campers:</Text>
+        <Picker
+          style={styles.formItem}
+          selectedValue={campers}
+          onValueChange={(itemValue) => setCampers(itemValue)}
+        >
+          <Picker.Item label="1" value={1} />
+          <Picker.Item label="2" value={2} />
+          <Picker.Item label="3" value={3} />
+          <Picker.Item label="4" value={4} />
+          <Picker.Item label="5" value={5} />
+          <Picker.Item label="6" value={6} />
+        </Picker>
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   formRow: {},
+  formLabel: {},
 });
 export default ReservationsScreen;
