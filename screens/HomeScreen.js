282916900616839e24973220dc7ef68590fb62c7
omaran bazna <omran4d@gmail.com>
2022-09-10 11:26:03 -0400

    solve an issue

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -35,7 +35,8 @@ const HomeScreen = () => {
   const promotions = useSelector((state) => state.promotions);
   const partners = useSelector((state) => state.partners);
 
-  const scaleValue = useRef(new Animated.Value(0).current);
+  const scaleValue = useRef(new Animated.Value(0)).current;
+
   const scaleAnimation = Animated.timing(scaleValue, {
     toValue: 1,
     duration: 1500,
