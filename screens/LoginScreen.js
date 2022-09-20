5c96bf8f38987d93f3289ac77ea8996c4ed08936
omaran bazna <omran4d@gmail.com>
2022-09-20 09:59:48 -0400

    create the login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+import { View, Button, StyleSheet } from "react-native";
+
+import { CheckBox, Input } from "react-native-elements";
+import * as SecureStore from "expo-secure-store";
