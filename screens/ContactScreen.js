d0c154925b422ada6c92a7f83fb5504c425de420
omaran bazna <omran4d@gmail.com>
2022-09-20 13:04:30 -0400

    handle send mail

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 const ContactScreen = () => {
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      recipients: ["campsite@info.com"],
+      subject: "subject",
+      body: "to whom he may concern",
+    });
+  };
   return (
     <ScrollView>
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -15,6 +23,19 @@ const ContactScreen = () => {
           <Text> </Text>
           <Text>Phone:+1 312 231 2131</Text>
           <Text>Email: someone@somedomin.somthing</Text>
+          <Button
+            title="Send Email"
+            buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
+            icon={
+              <Icon
+                name="envelope-o"
+                type="font-awesome"
+                color="#fff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            onPress={() => sendMail()}
+          />
         </Card>
       </Animatable.View>
     </ScrollView>
